c907bf3bacd78a7d8727fbe442f20b8d8804c49d
Julia Czosnek <julia.cz13@o2.pl>
2022-12-20 18:44:14 +0100

    Lesson 3 Task 4

--- src/main/java/org/futurecollars/lesson3task2/Film.java
+++ src/main/java/org/futurecollars/lesson3task2/Film.java
@@ -1,8 +1,15 @@
 package org.futurecollars.lesson3task2;
 
 public class Film {
-    private String filmTitle;
-    private String directorNameAndLastName;
-    private int timeInMinutes;
-    private int YearOfRelease;
+    private final String title;
+    private final String director;
+    private final int timeInMinutes;
+    private final int yearOfRelease;
+
+    public Film(String title, String director, int timeInMinutes, int yearOfRelease) {
+        this.title = title;
+        this.director = director;
+        this.timeInMinutes = timeInMinutes;
+        this.yearOfRelease = yearOfRelease;
+    }
 }
