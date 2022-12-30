0d1d8ce8978a883b678dac78f4f92e13b3b7ecd0
Julia Czosnek <julia.cz13@o2.pl>
2022-12-30 19:22:44 +0100

    Lesson4 Task2

---
+++ src/main/java/org/futurecollars/lesson4/task2/Square.java
@@ -0,0 +1,18 @@
+package org.futurecollars.lesson4.task2;
+
+public class Square extends Rectangle{
+
+    public Square(double sideA) {
+        super(sideA, sideA);
+    }
+
+    @Override
+    public double getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return super.getPerimeter();
+    }
+}
