42fc8f1c56296b1b9f72eabf43fddcd0972452ae
Julia Czosnek <julia.cz13@o2.pl>
2022-12-30 23:07:50 +0100

    Lesson4 Task9

---
+++ src/main/java/org/futurecollars/lesson4/task9/Triangle.java
@@ -0,0 +1,19 @@
+package org.futurecollars.lesson4.task9;
+
+public class Triangle {
+    private final int width;
+    private final int height;
+
+    public Triangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
