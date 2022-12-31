f91cff60d1b79f77ef404b447129bc24e044d6fd
Julia Czosnek <julia.cz13@o2.pl>
2022-12-31 11:37:30 +0100

    Updated Lesson4 Task1

--- src/main/java/org/futurecollars/lesson4/task1/ConsoleMessage.java
+++ src/main/java/org/futurecollars/lesson4/task1/ConsoleMessage.java
@@ -1,6 +1,6 @@
 package org.futurecollars.lesson4.task1;
 
-public class ConsoleMessage {
+public class ConsoleMessage extends Message {
     public String getMessage() {
         return "This is class ConsoleMessage";
     }
--- src/main/java/org/futurecollars/lesson4/task1/WindowMessage.java
+++ src/main/java/org/futurecollars/lesson4/task1/WindowMessage.java
@@ -1,6 +1,6 @@
 package org.futurecollars.lesson4.task1;
 
-public class WindowMessage {
+public class WindowMessage extends Message{
     public String getMessage() {
         return "This is class WindowMessage";
     }
