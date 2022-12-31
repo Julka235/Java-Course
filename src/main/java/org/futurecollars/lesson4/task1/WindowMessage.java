04b76f0a9997960b0ca2125fc77bea013659aff7
Julia Czosnek <julia.cz13@o2.pl>
2022-12-31 11:40:17 +0100

    Final version Lesson4 Task1

--- src/main/java/org/futurecollars/lesson4/task1/WindowMessage.java
+++ src/main/java/org/futurecollars/lesson4/task1/WindowMessage.java
@@ -1,6 +1,7 @@
 package org.futurecollars.lesson4.task1;
 
 public class WindowMessage extends Message{
+    @Override
     public String getMessage() {
         return "This is class WindowMessage";
     }
