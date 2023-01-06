0f6fc54975e14d0c55d0582cbf7db26f5fad7712
Julia Czosnek <julia.cz13@o2.pl>
2023-01-06 18:08:35 +0100

    Lesson6 Task1

---
+++ src/main/java/org/futurecollars/lesson6/task1/PrintFile.java
@@ -0,0 +1,20 @@
+package org.futurecollars.lesson6.task1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class PrintFile {
+    public static void main(String[] args) {
+        printFile("C:\\Users\\MyName\\data.txt");
+    }
+
+    public static void printFile(String pathToFile) {
+        try {
+            String fileContent = Files.readString(Path.of(pathToFile));
+            System.out.println(fileContent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
