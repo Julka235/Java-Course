7afb2ca474c1a8fbaa8e3a4e5ac0f497779368f2
Julia Czosnek <julia.cz13@o2.pl>
2023-01-06 18:52:59 +0100

    Lesson6 Task4

---
+++ src/main/java/org/futurecollars/lesson6/task4/SearchForWord.java
@@ -0,0 +1,22 @@
+package org.futurecollars.lesson6.task4;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SearchForWord {
+    public static void main(String[] args) {
+        String pathToFile = "C:\\Users\\julia\\Desktop\\data.txt";
+        System.out.println(doesContainSearchedWord(pathToFile, "Java"));
+        System.out.println(doesContainSearchedWord(pathToFile, "Coffee"));
+    }
+
+    public static boolean doesContainSearchedWord(String pathToFile, String searchedWord) {
+        try {
+            String fileContent = Files.readString(Path.of(pathToFile));
+            return fileContent.contains(searchedWord);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
