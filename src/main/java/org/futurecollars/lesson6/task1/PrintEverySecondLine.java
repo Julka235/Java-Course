696ffbbc99cf1dafa8bc3188667b95f702820913
Julia Czosnek <julia.cz13@o2.pl>
2023-01-06 18:24:24 +0100

    Lesson6 Task2

---
+++ src/main/java/org/futurecollars/lesson6/task1/PrintEverySecondLine.java
@@ -0,0 +1,23 @@
+package org.futurecollars.lesson6.task1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class PrintEverySecondLine {
+    public static void main(String[] args) {
+        printEverySecondLine("C:\\Users\\MyName\\data.txt");
+    }
+
+    public static void printEverySecondLine(String pathToFile) {
+        try {
+            String fileContent = Files.readString(Path.of(pathToFile));
+            String[] lines = fileContent.split("\n");
+            for(int i=0; i < 7; i+=2) {
+                System.out.println(lines[i]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
