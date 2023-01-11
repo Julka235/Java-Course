95206768a59f990fb1fcf135143f7dbc2465481b
Julia Czosnek <julia.cz13@o2.pl>
2023-01-11 17:00:54 +0100

    Lesson5 Task3

---
+++ src/main/java/org/futurecollars/lesson5/task3/Calculator.java
@@ -0,0 +1,19 @@
+package org.futurecollars.lesson5.task3;
+
+public class Calculator {
+    public static int add(int firstValue, int secondValue) {
+        return firstValue + secondValue;
+    }
+
+    public static int subtract(int firstValue, int secondValue) {
+        return firstValue - secondValue;
+    }
+
+    public static int multiply(int firstValue, int secondValue) {
+        return firstValue * secondValue;
+    }
+
+    public static int divide(int firstValue, int secondValue) {
+        return firstValue / secondValue;
+    }
+}
