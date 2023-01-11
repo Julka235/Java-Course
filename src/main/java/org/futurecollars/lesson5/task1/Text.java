15e41dd7bd11a87368d6a062f19754023cbfe413
Julia Czosnek <julia.cz13@o2.pl>
2023-01-11 16:20:24 +0100

    Lesson5 Task1

---
+++ src/main/java/org/futurecollars/lesson5/task1/Text.java
@@ -0,0 +1,22 @@
+package org.futurecollars.lesson5.task1;
+
+public class Text {
+    private static boolean isPalindrome(String text) {
+        if(text == null || text.equals("")) return false;
+        String cleanText = text.replaceAll("\\s+", "").toLowerCase();
+        int forward = 0;
+        int backward = cleanText.length() - 1;
+        while (backward > forward) {
+            char forwardChar = cleanText.charAt(forward++);
+            char backwardChar = cleanText.charAt(backward--);
+            if (forwardChar != backwardChar)
+                return false;
+        }
+        return true;
+    }
+
+    private static int getTextLength(String text) {
+        if(text == null || text.equals("")) return -1;
+        return text.length();
+    }
+}
