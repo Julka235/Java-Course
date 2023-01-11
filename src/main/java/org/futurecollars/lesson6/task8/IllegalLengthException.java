b092d473813a398780c4e662362726d83b6430ee
Julia Czosnek <julia.cz13@o2.pl>
2023-01-11 18:45:44 +0100

    Lesson6 Task8

---
+++ src/main/java/org/futurecollars/lesson6/task8/IllegalLengthException.java
@@ -0,0 +1,7 @@
+package org.futurecollars.lesson6.task8;
+
+public class IllegalLengthException extends RuntimeException{
+    public IllegalLengthException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/futurecollars/lesson6/task8/WrongTypeOfDataException.java
@@ -0,0 +1,7 @@
+package org.futurecollars.lesson6.task8;
+
+public class WrongTypeOfDataException extends RuntimeException{
+    public WrongTypeOfDataException(String message) {
+        super(message);
+    }
+}
