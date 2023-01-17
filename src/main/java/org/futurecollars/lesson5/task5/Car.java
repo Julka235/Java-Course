67bf74bef4832b5775ee7d7d15fea4bbac85e026
Julia Czosnek <julia.cz13@o2.pl>
2023-01-17 18:36:55 +0100

    Hopefully final version Lesson5 Task5

--- src/main/java/org/futurecollars/lesson5/task5/Car.java
+++ src/main/java/org/futurecollars/lesson5/task5/Car.java
@@ -3,4 +3,20 @@ package org.futurecollars.lesson5.task5;
 
 public class Car extends Vehicle {
     private Engine engine;
+
+    private Car(Engine engine){
+        this.engine = engine;
+    }
+
+    public boolean isEngineStarted() {
+        return engine.isStarted();
+    }
+
+    public void startEngine() {
+        engine.start();
+    }
+
+    public void stopEngine() {
+        engine.stop();
+    }
 }
--- src/main/java/org/futurecollars/lesson5/task5/Engine.java
+++ src/main/java/org/futurecollars/lesson5/task5/Engine.java
@@ -3,21 +3,17 @@ package org.futurecollars.lesson5.task5;
 public class Engine {
     private boolean isStarted;
 
-    public void setStateOfTheEngine(boolean started) {
-        this.isStarted = started;
-    }
-
     public boolean isStarted() {
         return isStarted;
     }
 
-    public void startEngine() {
+    public void start() {
         if (!isStarted) {
             isStarted = true;
         }
     }
 
-    public void stopEngine() {
+    public void stop() {
         if (isStarted) {
             isStarted = false;
         }
