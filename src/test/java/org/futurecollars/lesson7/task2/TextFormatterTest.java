caaf83fae160e8916ab9898ef9265489eb24b60e
Julia Czosnek <julia.cz13@o2.pl>
2023-01-19 19:01:15 +0100

    Updated Lesson7 Task2

--- src/test/java/org/futurecollars/lesson7/task2/TextFormatterTest.java
+++ src/test/java/org/futurecollars/lesson7/task2/TextFormatterTest.java
@@ -2,19 +2,22 @@ package org.futurecollars.lesson7.task2;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class TextFormatterTest {
     @ParameterizedTest
     @ValueSource(strings = {"cat", "King", "NASA", "isTRUE"})
-    @NullAndEmptySource
-    void shouldReturnTextInUpperCase(String input) {
-        String expectedText;
-        if(input == null) expectedText = null;
-        else expectedText = input.toUpperCase();
+    @EmptySource
+    void shouldReturnTextInUpperCaseOrEmptyForEmpty(String input) {
+        String expectedText = input.toUpperCase();
         Assertions.assertEquals(expectedText, TextFormatter.getTextInUpperCase(input));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void shouldReturnNullForNull(String input) {
+        Assertions.assertNull(TextFormatter.getTextInUpperCase(input));
     }
 }
