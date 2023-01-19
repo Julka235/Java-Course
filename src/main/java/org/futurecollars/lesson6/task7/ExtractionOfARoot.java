c2499e994a957d127727ba5b4ce487b81a6b3992
Julia Czosnek <julia.cz13@o2.pl>
2023-01-19 19:21:43 +0100

    Final Version Lesson6 Task7

--- src/main/java/org/futurecollars/lesson6/task7/ExtractionOfARoot.java
+++ src/main/java/org/futurecollars/lesson6/task7/ExtractionOfARoot.java
@@ -3,10 +3,6 @@ package org.futurecollars.lesson6.task7;
 import static java.lang.Math.sqrt;
 
 public class ExtractionOfARoot {
-    public static void main(String[] args) {
-        System.out.println(extractTheRootOf(-1));
-    }
-
     public static double extractTheRootOf(double number) {
         if(number < 0) throw new IllegalArgumentException("Number cannot be negative.");
         return sqrt(number);
