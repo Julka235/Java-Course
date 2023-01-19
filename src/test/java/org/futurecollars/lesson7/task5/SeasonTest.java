8e8cfbec675848528a012a9a260be8a38f3e5cc7
Julia Czosnek <julia.cz13@o2.pl>
2023-01-19 19:10:45 +0100

    Final Version Lesson7 Task5

--- src/test/java/org/futurecollars/lesson7/task5/SeasonTest.java
+++ src/test/java/org/futurecollars/lesson7/task5/SeasonTest.java
@@ -8,8 +8,26 @@ import java.time.Month;
 
 class SeasonTest {
     @ParameterizedTest
-    @CsvSource(value = {"APRIL:SPRING", "DECEMBER:WINTER", "SEPTEMBER:AUTUMN", "JULY:SUMMER"}, delimiter = ':')
-    void shouldReturnSeasonForMonth(Month month, Season season) {
-        Assertions.assertEquals(season, Season.getSeason(month));
+    @CsvSource(value = {"DECEMBER", "JANUARY", "FEBRUARY"})
+    void shouldReturnWinter(Month month) {
+        Assertions.assertEquals(Season.WINTER, Season.getSeason(month));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"MARCH", "APRIL", "MAY"})
+    void shouldReturnSpring(Month month) {
+        Assertions.assertEquals(Season.SPRING, Season.getSeason(month));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"JUNE", "JULY", "AUGUST"})
+    void shouldReturnSummer(Month month) {
+        Assertions.assertEquals(Season.SUMMER, Season.getSeason(month));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"SEPTEMBER", "OCTOBER", "NOVEMBER"})
+    void shouldReturnAutumn(Month month) {
+        Assertions.assertEquals(Season.AUTUMN, Season.getSeason(month));
     }
 }
