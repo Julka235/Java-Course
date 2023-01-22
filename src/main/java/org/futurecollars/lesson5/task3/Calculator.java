88de1c7f7300186d4dac49969048786533251b92
Julia Czosnek <julia.cz13@o2.pl>
2023-01-22 13:59:48 +0100

    Lesson5 Task3

--- src/main/java/org/futurecollars/lesson5/task3/Calculator.java
+++ src/main/java/org/futurecollars/lesson5/task3/Calculator.java
@@ -1,24 +1,18 @@
 package org.futurecollars.lesson5.task3;
 
 public class Calculator {
-    public static void main(String[] args) {
-        Operation[] operations = { new Addition(), new Subtraction(), new Multiplication(), new Division() };
-        Calculator calculator = new Calculator(operations);
-        System.out.println(calculator.compute(7, 2, '-'));
-    }
     private final Operation[] operations;
 
     public Calculator(Operation[] operations) {
         this.operations = operations;
     }
 
-    public int compute(int firstValue, int secondValue, char operationSign) {
-        for(int i=0; i<operations.length; i++) {
-            Operation operation = operations[i];
-            if(operation.getSign() == operationSign) {
+    public int compute(int firstValue, char operationSign, int secondValue) {
+        for (Operation operation : operations) {
+            if (operation.getSign() == operationSign) {
                 return operation.compute(firstValue, secondValue);
             }
         }
-        throw new IllegalArgumentException("...");
+        throw new IllegalArgumentException("No operation matches the sign.");
     }
 }
