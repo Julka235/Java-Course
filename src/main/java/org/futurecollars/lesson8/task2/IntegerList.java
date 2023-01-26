e1564b5f9c6c4d292bf89671d69e377b5624432e
Julia Czosnek <julia.cz13@o2.pl>
2023-01-26 16:56:55 +0100

    Updated Lesson8 Task2

--- src/main/java/org/futurecollars/lesson8/task2/IntegerList.java
+++ src/main/java/org/futurecollars/lesson8/task2/IntegerList.java
@@ -1,33 +1,22 @@
 package org.futurecollars.lesson8.task2;
 
-public class IntegerList implements OwnList{
-    Integer[] integerList;
-    int n;
+public class IntegerList implements OwnList {
+    private Integer[] integerList;
+    private int listSize;
 
     public IntegerList() {
-        this.n = 0;
-        this.integerList = new Integer[n];
+        this.listSize = 0;
+        this.integerList = new Integer[10];
     }
 
     @Override
     public int size() {
-        return n;
+        return listSize;
     }
 
     @Override
     public boolean isEmpty() {
-        return n == 0;
-    }
-
-    @Override
-    public void add(Integer element) {
-        Integer[] newIntegerList = new Integer[n + 1];
-        for(int i = 0; i < n; i++) {
-            newIntegerList[i] = integerList[i];
-        }
-        newIntegerList[n] = element;
-        integerList = newIntegerList;
-        n++;
+        return listSize == 0;
     }
 
     @Override
@@ -36,39 +25,58 @@ public class IntegerList implements OwnList{
     }
 
     @Override
+    public void add(Integer element) {
+        if (listSize % 10 == 0) {
+            Integer[] newIntegerList = new Integer[listSize + 10];
+            for (int i = 0; i < listSize; i++) {
+                newIntegerList[i] = integerList[i];
+            }
+            integerList = newIntegerList;
+        }
+        integerList[listSize] = element;
+        listSize++;
+    }
+
+    @Override
     public void add(int index, Integer element) {
-        if(index < 0 || index >= n) {
+        if (index < 0 || index >= listSize) {
             throw new ArrayIndexOutOfBoundsException("Such an index does not exist.");
         }
 
-        Integer[] newIntegerList = new Integer[n + 1];
-        for(int i = 0; i < index; i++) {
-            newIntegerList[i] = integerList[i];
-        }
-        newIntegerList[index] = element;
-        for(int i = index + 1; i < n + 1; i++) {
-            newIntegerList[i] = integerList[i-1];
+        if (listSize % 10 == 0) {
+            Integer[] newIntegerList = new Integer[listSize + 10];
+            for (int i = 0; i < listSize; i++) {
+                newIntegerList[i] = integerList[i];
+            }
+            integerList = newIntegerList;
         }
 
-        integerList = newIntegerList;
-        n++;
+        for (int i = listSize; i > index; i--) {
+            integerList[i] = integerList[i - 1];
+        }
+        integerList[index] = element;
+        listSize++;
     }
 
     @Override
     public void remove(int index) {
-        if(index < 0 || index >= n) {
+        if (index < 0 || index >= listSize) {
             throw new ArrayIndexOutOfBoundsException("Such an index does not exist.");
         }
 
-        Integer[] newIntegerList = new Integer[n - 1];
-        for(int i = 0; i < index; i++) {
-            newIntegerList[i] = integerList[i];
-        }
-        for(int i = index; i < n - 1; i++) {
-            newIntegerList[i] = integerList[i + 1];
+        if (integerList.length > listSize * 2) {
+            int newListLength = integerList.length / 2 + 10;
+            newListLength = newListLength - (newListLength % 10);
+            Integer[] newIntegerList = new Integer[newListLength];
+            for (int i = 0; i < listSize; i++) {
+                newIntegerList[i] = integerList[i];
+            }
+            integerList = newIntegerList;
         }
 
-        integerList = newIntegerList;
-        n--;
+        for (int i = index; i < listSize - 1; i++) {
+            integerList[i] = integerList[i + 1];
+        }
+        listSize--;
     }
 }
